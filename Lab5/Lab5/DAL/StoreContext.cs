08a594ec074f856f905b04d7ae717e94b298accf
thaivinh088 <thaivinhnguyen088@gmail.com>
2022-04-12 17:18:58 +0700

    1

---
+++ Lab5/Lab5/DAL/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Lab5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab5.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext() : base("name=StoreDBConnection")
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Lab5/Lab5/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab5.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public float ItemPrice { get; set; }
+        public float ItemWeight { get; set; }
+        public byte[] ItemPhoto { get; set; }
+        //Khoa ngoai 
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
